41854ecebc30a834b64c080082b4bf9f58c11ca8
Yogesh <eyogesh104@gmail.com>
2025-11-29 11:35:53 +0530

    update upstash workflow

--- config/upstash.js
+++ config/upstash.js
@@ -3,12 +3,12 @@ import { QSTASH_URL, QSTASH_TOKEN } from "./env.js";
 
 // Create workflow client with development configuration
 export const workflowClient = new WorkflowClient({
-  baseUrl: QSTASH_URL || 'http://127.0.0.1:8080',
+  baseUrl: QSTASH_URL || 'http://127.0.0.1:8090',
   token: QSTASH_TOKEN || 'development',
 });
 
 // Log QStash configuration on startup
 console.log('QStash Configuration:', {
-  baseUrl: QSTASH_URL || 'http://127.0.0.1:8080',
+  baseUrl: QSTASH_URL || 'http://127.0.0.1:8090',
   hasToken: !!QSTASH_TOKEN
 });
